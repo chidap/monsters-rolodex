c8a99546ea7b91c52f788e145136b55468388742
Chida <chida_p@yahoo.com>
2019-09-15 22:34:29 +0530

    Updates

---
+++ precache-manifest.083f3d7cbe41baee0f477e8f16d6b4a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ebb3abbb636df93cbc2b4a2f7bc5f44",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "478b12e8d1d7764141d8",
+    "url": "/monsters-rolodex/static/css/main.6e4f00c5.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "478b12e8d1d7764141d8",
+    "url": "/monsters-rolodex/static/js/main.3646e21b.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
